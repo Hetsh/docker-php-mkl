b7aff57c56201e6bee1eeec5e3d774e0cf3f35d8
Hetsh <git@hetsh.de>
2024-01-22 05:08:36 +0100

    update.sh: don't include version in PHP package names

--- update.sh
+++ update.sh
@@ -16,13 +16,13 @@ source libs/docker.sh
 assert_dependency "jq"
 assert_dependency "curl"
 
-# Alpine Linux with PHP8.2-FPM
-update_image "hetsh/php82-fpm" "PHP8.2 FPM" "true" "(\d+\.)+\d+-\d+"
+# Alpine Linux with PHP-FPM
+update_image "hetsh/php82-fpm" "PHP FPM" "true" "(\d+\.)+\d+-\d+"
 
 # Packages
 PKG_URL="https://pkgs.alpinelinux.org/package/edge/community/x86_64"
-update_pkg "php82-ctype" "PHP8.2 CTYPE" "false" "$PKG_URL" "(\d+\.)+\d+-r\d+"
-update_pkg "php82-openssl" "PHP8.2 OPENSSL" "false" "$PKG_URL" "(\d+\.)+\d+-r\d+"
+update_pkg "php82-ctype" "PHP CTYPE" "false" "$PKG_URL" "(\d+\.)+\d+-r\d+"
+update_pkg "php82-openssl" "PHP OPENSSL" "false" "$PKG_URL" "(\d+\.)+\d+-r\d+"
 
 if ! updates_available; then
 	#echo "No updates available."
