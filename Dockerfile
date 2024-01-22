e7479350cbfbddd27bc4ce08722379de8f484ff6
Hetsh <git@hetsh.de>
2024-01-22 04:46:12 +0100

    PHP8.1 FPM 8.1.26-1 -> PHP8.2 FPM 8.2.15-1, PHP8.1 CTYPE 8.1.26-r0 -> PHP8.2 CTYPE 8.2.15-r0, PHP8.1 OPENSSL 8.1.26-r0 -> PHP8.2 OPENSSL 8.2.15-r0

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM hetsh/php81-fpm:8.1.26-1
+FROM hetsh/php82-fpm:8.2.15-1
 RUN apk add --no-cache \
-        php81-ctype=8.1.26-r0 \
-        php81-openssl=8.1.26-r0
+        php82-ctype=8.2.15-r0 \
+        php82-openssl=8.2.15-r0
--- update.sh
+++ update.sh
@@ -16,13 +16,13 @@ source libs/docker.sh
 assert_dependency "jq"
 assert_dependency "curl"
 
-# Alpine Linux with PHP8.1-FPM
-update_image "hetsh/php81-fpm" "PHP8.1 FPM" "true" "(\d+\.)+\d+-\d+"
+# Alpine Linux with PHP8.2-FPM
+update_image "hetsh/php82-fpm" "PHP8.2 FPM" "true" "(\d+\.)+\d+-\d+"
 
 # Packages
 PKG_URL="https://pkgs.alpinelinux.org/package/edge/community/x86_64"
-update_pkg "php81-ctype" "PHP8.1 CTYPE" "false" "$PKG_URL" "(\d+\.)+\d+-r\d+"
-update_pkg "php81-openssl" "PHP8.1 OPENSSL" "false" "$PKG_URL" "(\d+\.)+\d+-r\d+"
+update_pkg "php82-ctype" "PHP8.2 CTYPE" "false" "$PKG_URL" "(\d+\.)+\d+-r\d+"
+update_pkg "php82-openssl" "PHP8.2 OPENSSL" "false" "$PKG_URL" "(\d+\.)+\d+-r\d+"
 
 if ! updates_available; then
 	#echo "No updates available."
